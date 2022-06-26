586e9c5a80f88feaf939c25f18466ec7d8482a9a
Jonathan Gonzalez <j@0x30.io>
2022-06-26 16:14:21 +0200

    example app source code

---
+++ src/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+""" SKEL """
+
+__author__ = "Jonathan Gonzalez"
+__email__ = "j@0x30.io"
+__date__ = "2022-04-25"
+
+
+__all__ = [
+    "__author__",
+    "__email__",
+    "__date__",
+]
---
+++ src/app.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+""" SKEL """
+
+# - Standard imports
+# - Third-party imports
+# - Local imports
+
+
+def main() -> None:
+    """Main program"""
+    print("This is the main program")
+
+
+if __name__ == "__main__":
+    main()
